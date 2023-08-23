a20b7f86a579f2f148666fb3d4e7a47a1b7442da
MmeliNtshakala <mmeliroot@gmail.com>
2023-08-23 08:39:32 -0700

    handle print

--- get_size.c
+++ get_size.c
@@ -12,9 +12,9 @@ int get_size(const char *format, int *i)
 	int curr_i = *i + 1;
 	int size = 0;
 	if (format[curr_i] == 'l')
-		size = SIZE_LONG;
+		size = S_LONG;
 	else if (format[curr_i] == 'h')
-		size = SIZE_SHORT;
+		size = S_SHORT;
 	if (size == 0)
 		*i = curr_i -1;
 	else
--- handle_print.c
+++ handle_print.c
@@ -15,7 +15,7 @@
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		int flags, int width, int precision, int size)
 {
-	int i, ubknown_len =0, printed_chars = -1;
+	int i, unknown_len =0, printed_chars = -1;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -25,7 +25,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 	{
-		if (fmt[*ind] == fmr+types[i].fmt)
+		if (fmt[*ind] == fmt+ types[i].fmt)
 			return fmt_types[i].fn(list, buffer, flags, width, precision, size);
 	}
 	if (fmt_types[i].fmt == '\0')
